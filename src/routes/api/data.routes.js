112256b67fa072cd4ffff70e3e51b26d93d6c654
chiquesricardochiques-sys <chiquesricardochiques@gmail.com>
2026-01-19 19:12:56 -0300

    Delete src directory

--- src/routes/api/data.routes.js
+++
@@ -1,9 +0,0 @@
-// import { Router } from "express";
-// import { getData, insertData } from "../../controllers/api/data.controller.js";
-
-// const router = Router();
-
-// router.get("/:project/:table", getData);
-// router.post("/:project/:table", insertData);
-
-// export default router;
