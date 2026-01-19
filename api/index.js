08dc9053cb059a44349f30b61b263c97a3d4ae07
chiquesricardochiques-sys <chiquesricardochiques@gmail.com>
2026-01-19 19:30:35 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,8 @@
 import app from "../src/app.js";
+import { createServer } from "http";
+import { VercelRequest, VercelResponse } from "@vercel/node";
 
-export default app;
+export default function handler(req, res) {
+  // Express já sabe lidar com req/res do Node
+  app(req, res);
+}
